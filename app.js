f90d989abd13659d2a07e5032aadfab74bc742a4
Faiz Ruzain <faiz.faiz8383@gmail.com>
2021-11-30 01:28:43 +0700

    removed unused code

--- app.js
+++ app.js
@@ -18,7 +18,6 @@ const port = process.env.PORT || 3000
 app.set('views', path.join(__dirname, './views'))
 app.set('view engine', 'ejs')
 
-// app.use(telegramBot)
 app.use(express.json());
 app.use(express.urlencoded({
   extended: true
